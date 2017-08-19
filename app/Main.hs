1f9a946c8fd08c28e4c65b5804382f21e50ce280
Mike Hewitt <matroxmike@icloud.com>
2017-08-20 07:02:08 +1000

    type signatures2

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,9 @@ module Main where
 
 import Lib
 
+prod :: Num a => a -> a -> a 
+prod a b = a * b
+
 sq :: Num a => a -> a
 sq x = x * x
 
@@ -14,5 +17,8 @@ main = do
     print $ (sq . sqrt) $ 7 + 9
     print $ (sqrt . id) 256
 -- 27.07.2017 02:06:23
+    print $ prod 4 5 
+    print $ prod  2 prod 4 5     
+    
     
     
