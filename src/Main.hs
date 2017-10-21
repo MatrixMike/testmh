f595ec30a228a803268ff42c2c79b4d3d2802626
Mike Hewitt <matroxmike@icloud.com>
2017-10-22 04:43:21 +1100

    test for importify

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,9 @@
 module Main where
+{-
+following line added so that importify can find it redundant and remove it
+-}
+
+import qualified Data.Monoid
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
